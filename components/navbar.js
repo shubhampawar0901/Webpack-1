0ef5bbcf4af1218291894ab5fe4473a576863a6d
Shubham Pawar <shubhampawar09011998@gmail.com>
2022-04-29 20:20:19 +0530

    f

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+let navbar = () => {
+    return`
+    <div id="options">
+    <a href="./daily.html">Receipe of The Day</a>
+    <a href="./index.html">More Receipes</a>
+    <input type="text" placeholder="Search Receipe" id="search" />
+    </div>
+    <div id="credentials">
+        <a href="login.html">Log in</a>
+        <a href="signup.html">Sign up</a>
+    </div>
+        `;
+    };
+    
+    export default navbar;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import navbar from '../components/navbar'
+import '../styles/style.css'
+// console.log('navbar:',navbar)
+
+let nav = document.getElementById("navbar");
+nav.innerHTML = navbar()
